2a28e48bbee1ab9022d58abd404505f21120df67
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-04-01 01:50:53 +0000

    Added loops section

---
+++ blackjack/aside-loops/loops.js
@@ -0,0 +1,41 @@
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//      START          FINISH      STEP SIZE
+for ( let count = 1;  count < 11;  count += 2 )  {
+
+    //console.log(count)
+
+}
+
+//Modify the count from 1 to 20
+
+for ( let count20 = 1;  count20 < 21;  count20 += 1 )  {
+
+  //console.log(count20)
+
+}
+
+//Modify the count from 10 to 20
+
+for ( let count10 = 10;  count10 < 21;  count10 += 1 )  {
+
+  //console.log(count10)
+
+}
+// Example loop 
+for (let iexample = 0; iexample < 6; iexample+=1) {
+  //console.log(iexample)
+}
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for ( var i = 10; i < 101; i += 10) {
+  console.log(i)
+}
