586435abc9c074f763574b071e41a42035c2fa87
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-04-29 22:41:31 +0000

    Updates for Mobile App

--- mobileapp/app.js
+++ mobileapp/app.js
@@ -1,21 +1,33 @@
+//Database Initalization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
- databaseURL: "https://player-tracker-123233-default-rtdb.firebaseio.com/"
+  databaseURL: "https://player-tracker-123233-default-rtdb.firebaseio.com/"
 }
 
+//Variables
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const playerListInDB = ref(database, "playerList")
-
 const addBtnEl = document.getElementById("add-button")
-const inputEl = document.getElementById("input-field")
+const inputFieldEl = document.getElementById("input-field")
+const playerListEl = document.getElementById("player-list")
 
+
+//Events
 addBtnEl.addEventListener("click", function() {
-  let inputValue = inputEl.value;
-
-push(playerListInDB, inputValue)
+  let inputValue = inputFieldEl.value;
+  push(playerListInDB, inputValue)
+  clearInputField()
   
-  console.log(inputValue);
+  appendItemToPlayerListEl(inputValue) //Paramater to give the function appendItemToPlayerListEl value of inputValu
 });
+
+function clearInputField(){
+  inputFieldEl.value = ""
+}
+
+function appendItemToPlayerListEl(itemValue){
+  playerListEl.innerHTML += `<li>${itemValue}</li>`
+}
