42051a78de45ce10e919b7c6fed49b786b7ac4a9
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-04-30 23:12:57 +0000

    Updates

--- mobileapp/app.js
+++ mobileapp/app.js
@@ -1,12 +1,12 @@
-//Database Initalization
+//Database Initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://player-tracker-123233-default-rtdb.firebaseio.com/"
 }
 
-//Variables
+// Variables
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const playerListInDB = ref(database, "playerList")
@@ -14,20 +14,36 @@ const addBtnEl = document.getElementById("add-button")
 const inputFieldEl = document.getElementById("input-field")
 const playerListEl = document.getElementById("player-list")
 
-
-//Events
+// Event listener for Add button
 addBtnEl.addEventListener("click", function() {
-  let inputValue = inputFieldEl.value;
-  push(playerListInDB, inputValue)
-  clearInputField()
+  let inputValue = inputFieldEl.value; // Gets Value from INPUT FIELD
+  push(playerListInDB, inputValue); // PUSHES input field data to database
   
-  appendItemToPlayerListEl(inputValue) //Paramater to give the function appendItemToPlayerListEl value of inputValu
+  clearInputField();
 });
 
-function clearInputField(){
-  inputFieldEl.value = ""
+// Event listener for database changes
+onValue(playerListInDB, function(snapshot) {
+  let itemsArray = Object.values(snapshot.val()); // Convert snapshot values to array
+  for (let i = 0; i < itemsArray.length; i++) {
+    appendItemToPlayerListEl(itemsArray[i]); // Append each item to player list
+  }
+  
+  clearPlayerListEl()
+})
+
+
+
+// Function to clear the input field
+function clearInputField() {
+  inputFieldEl.value = "";
 }
 
-function appendItemToPlayerListEl(itemValue){
-  playerListEl.innerHTML += `<li>${itemValue}</li>`
+// Function to append an item to the player list element
+function appendItemToPlayerListEl(itemValue) {
+  playerListEl.innerHTML += `<li>${itemValue}</li>`;
+}
+
+function clearPlayerListEl() {
+  playerListEl.innerHTML = "";
 }
