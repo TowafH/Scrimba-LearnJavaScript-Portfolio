52f0990ddbdecd9e31ad3c35cd819095180f0063
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-05-02 23:33:02 +0000

    Updates

--- mobileapp/app.js
+++ mobileapp/app.js
@@ -1,6 +1,6 @@
 //Database Initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://player-tracker-123233-default-rtdb.firebaseio.com/"
@@ -18,32 +18,55 @@ const playerListEl = document.getElementById("player-list")
 addBtnEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value; // Gets Value from INPUT FIELD
   push(playerListInDB, inputValue); // PUSHES input field data to database
-  
+
   clearInputField();
-});
+})
 
 // Event listener for database changes
 onValue(playerListInDB, function(snapshot) {
-  let itemsArray = Object.values(snapshot.val()); // Convert snapshot values to array
-  for (let i = 0; i < itemsArray.length; i++) {
-    appendItemToPlayerListEl(itemsArray[i]); // Append each item to player list
+
+  if (snapshot.exists()){
+    let itemsArray = Object.entries(snapshot.val()); // Convert snapshot values to array
+    clearPlayerListEl()
+
+    for (let i = 0; i < itemsArray.length; i++) {
+
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1]
+
+      appendItemToPlayerListEl(currentItem); // Append each item to player list
+    }
+  } else {
+    playerListEl.innerHTML = "No Items Here Yet"
   }
-  
-  clearPlayerListEl()
+
 })
 
 
+function clearPlayerListEl() {
+  playerListEl.innerHTML = "";
+}
 
-// Function to clear the input field
 function clearInputField() {
   inputFieldEl.value = "";
 }
 
 // Function to append an item to the player list element
-function appendItemToPlayerListEl(itemValue) {
-  playerListEl.innerHTML += `<li>${itemValue}</li>`;
-}
+function appendItemToPlayerListEl(item) {
 
-function clearPlayerListEl() {
-  playerListEl.innerHTML = "";
+  let itemID = item[0]
+  let itemValue = item[1]
+
+  let newEl = document.createElement("li")
+
+  newEl.textContent = itemValue
+
+  newEl.addEventListener("click", function() {
+  let exactLocationOfItemInDB = ref(database, `playerList/${itemID}`)
+
+  remove(exactLocationOfItemInDB)
+  })
+  
+  playerListEl.append(newEl)
 }
